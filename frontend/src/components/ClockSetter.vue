2961b716e8d95a3d7e37ef57203f0a2557c8e61a
PFinal_南丞 <1370288056@qq.com>
2024-08-26 19:21:39 +0800

    更新

--- frontend/src/components/ClockSetter.vue
+++ frontend/src/components/ClockSetter.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="containerClass">
-        <span :class="tipsClass">{{ tips }}</span>
+        <span :class="[tipsClass,'marquee']">{{ tips }}</span>
         <span :class="iconClass"></span> 
     </div>
 </template>
@@ -13,12 +13,25 @@ export default {
             containerClass: 'w-11/12 h-10 bg-gray-200 rounded-lg flex justify-center items-center m-auto',
             iconClass: 'icon-[material-symbols--settings] text-blue-300',
             tips: "PFinalClub",
-            tipsClass: "w-5/6 text-black-500 text-center"
+            tipsClass: "w-5/6 text-black-500 inline-block whitespace-nowrap"
         }
     }
 }
 </script>
 
 <style scoped>
+.marquee {
+    overflow: hidden;
+    animation: marquee 10s linear infinite;
+}
+
+@keyframes marquee {
+    from {
+        transform: translateX(100%);
+    }
+    to {
+        transform: translateX(-100%);
+    }
+}
 /* 如果有需要，可以在此添加样式 */
 </style>
